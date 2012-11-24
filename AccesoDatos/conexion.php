3a17e3d3866060b82990764e873559b663422cea
maleja <mayraata@s3pc6.eisc.univalle.edu.co>
2012-11-24 15:52:24 -0500

    agregue conexion

---
+++ AccesoDatos/conexion.php
@@ -0,0 +1,69 @@
+<?php
+
+class Conexion {
+
+    private $usuario;
+    private $password;
+    private $basedatos;
+    private $server;
+    private $conexion;
+
+    function Conexion() {
+        $this->server = "mysql";
+        $this->usuario = "mayraata";
+        $this->password = "mayraata";
+        $this->basedatos = "mayraata";
+      }
+        
+        
+
+        //echo "asf";
+    
+
+    /*function Conectar() {
+        if ($this->conexion != NULL) {
+            $this->conexion = mysql_connect($this->server, $this->usuario, $this->password);
+            $bool = mysql_select_db($this->basedatos, $this->conexion);
+            return $this->conexion;
+            if ($bool === False) {
+                print "can't find $mysql_database";
+            }
+        } else {
+            return $this->conexion;
+        }
+    }*/
+    
+   
+  function Conectar(){
+ 
+   
+    $this->conexion = mysql_connect($this->server, $this->usuario,  $this->password);
+    mysql_select_db($this->basedatos,$this->conexion);
+    return $this->conexion;
+  }
+    
+  //Realizar una consulta o actualización en la BD
+  function EjecutarSql($consulta){  
+     $this->resconsul      = mysql_query($consulta);
+ 	 $this->registroActual = mysql_fetch_array($this->resconsul);
+	 return $this->registroActual;
+  } 
+
+    //Devuelve el ultimo mensaje de error
+    function Error() {
+        return mysql_error();
+    }
+
+//Finalizar la conexion con el servidor
+    function Cerrar() {
+        mysql_close();
+    }
+    
+    //Ejecuta sentencias
+    function executeQuery($sqr){
+        
+    }
+
+}
+
+?>
