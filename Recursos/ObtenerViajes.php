96307418b1da692aaa9f4ef4a76fd47de67f76f5
Jhonathan <jhota90@gmail.com>
2012-11-25 17:25:29 -0500

    muestra id viaje

--- Recursos/ObtenerViajes.php
+++ Recursos/ObtenerViajes.php
@@ -1,15 +1,28 @@
 <?php
+
 include_once '../AccesoDatos/conexion.php';
+$opcion = $_GET['opcion'];
 
 $conexion = new Conexion();
-$conexion->Conectar();
 
-$sql = "SELECT * FROM registro_viajes";
-$respuesta = mysql_query($sql);
-$filas = array();
-while ($row = mysql_fetch_array($respuesta)) {
-    $filas [] = $row;
+
+if ($opcion == 1) {
+    $conexion->Conectar();
+    $sql = "SELECT * FROM registro_viajes";
+    $respuesta = mysql_query($sql);
+    $filas = array();
+    while ($row = mysql_fetch_array($respuesta)) {
+        $filas [] = $row;
+    }
+    $conexion->Cerrar();
+    echo json_encode($filas);
 }
-$conexion->Cerrar();
-echo json_encode($filas);
+
+if ($opcion == 2) {
+    $viaje = $_GET['viaje'];
+    $conexion->Conectar();
+    $sql = "DELETE FROM registro_viajes WHERE codigoViaje=" . $viaje;
+    $respuesta = mysql_query($sql);
+    $conexion->Cerrar();
+}
 ?>
--- Recursos/manejaTabla.js
+++ Recursos/manejaTabla.js
@@ -1,16 +1,13 @@
- /* <?php
-                            include_once 'Recursos/Consultar.php';
-                            $obtenerViajes = new ObtenerViajes();
+function eliminar(codigoViaje){
+    alert(codigoViaje);
+}
 
-                            $listasViajes = $obtenerViajes->obtenerListaViajes();
+function modificar(codigoViaje){
+    alert(codigoViaje);
+}
 
-                            for ($index = 0; $index < count($listasViajes); $index++) {
-                                echo "<tr><td>" . $listasViajes[$index][0] . "</td>
-                                    <td>" . $listasViajes[$index][1] . "</td> <td>" . $listasViajes[$index][2] . "</td><td>" . $listasViajes[$index][3] . "</td><td>" . $listasViajes[$index][4] . "</td><td>" . $listasViajes[$index][5] . "</td><td>" . $listasViajes[$index][6] . "</td></tr>";
-                            }
-                            ?>*/
 $(function(){
-    var urlPhp="Recursos/ObtenerViajes.php";
+    var urlPhp="Recursos/ObtenerViajes.php?opcion=1";
     
     $.ajax({
         type: 'POST',
@@ -21,10 +18,41 @@ $(function(){
             for(var i=0 ; i < result.length ; i++){
                 var contenidoHtml = "";
                 var unaFila = result[i];
-                contenidoHtml += "<tr><td>"+ unaFila[0]+"</td><td>"+unaFila[1]+"</td> <td>"+unaFila[2]+"</td><td>"+unaFila[3]+"</td><td>"+unaFila[4]+"</td><td>" +unaFila[5]+"</td><td>"+unaFila[6]+"</td></tr>";
+                contenidoHtml += "<tr><td>"+ unaFila[0]+"</td><td>"+unaFila[1]+"</td> <td>"+unaFila[2]+"</td><td>"+unaFila[3]+"</td><td>"+unaFila[4]+"</td><td>" +unaFila[5]+"</td><td>"+unaFila[6]+"</td><td><a href='' onclick='eliminar("+unaFila[0]+")'>Eliminar</a></br><a href='' onclick='modificar("+unaFila[0]+")'>Modificar</a></td></tr>";
                 $('#tabla').append(contenidoHtml);                
             }
         }     
     }
     );
+   
+    var listaViajes = $('tr');
+    
+    listaViajes.on('click',function(){
+        
+              alert("hola");
+        var accion = 0;
+        if($(this).text()=="Eliminar"){
+            accion = 1;
+        }else if($(this).text()=="Modificar"){
+            accion=2;
+        }
+        var id=$(this).attr('id');
+  
+        
+        if(accion==1){
+          /*var urlEliminar="Recursos/ObtenerViajes.php?opcion=2&viaje=";    
+            $.ajax({
+                type: 'POST',
+                url: urlEliminar,
+                success: function(result) {
+                }     
+            }
+            ); */
+        }
+        if(accion==2){
+            
+        }
+        });
+    
+           
 });
