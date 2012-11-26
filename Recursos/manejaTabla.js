6c6064590a236b586476b5e422c3abe29861bd3c
Jhonathan <jhota90@gmail.com>
2012-11-25 20:50:36 -0500

    index

--- Recursos/manejaTabla.js
+++ Recursos/manejaTabla.js
@@ -12,22 +12,15 @@ function insertar(){
     var hora = $('hora').val();
     var min = $('min').val();
     var estacionOrigen = $('estO').val();
-    var estacionDestino = $('estD').val();    
+    var estacionDestino = $('estD').val();
     
-    var urlPhp="Recursos/ObtenerViajes.php?opcion=1";
+    var urlPhp="Recursos/crudViajes.php?opcion=1";
     $.ajax({
         type: 'POST',
         url: urlPhp,
         dataType: 'json',
         cache: false,
-        success: function(result) {
-            for(var i=0 ; i < result.length ; i++){
-                var contenidoHtml = "";
-                var unaFila = result[i];
-                contenidoHtml += "<tr><td>"+ unaFila[0]+"</td><td>"+unaFila[1]+"</td> <td>"+unaFila[2]+"</td><td>"+unaFila[3]+"</td><td>"+unaFila[4]+"</td><td>" +unaFila[5]+"</td><td>"+unaFila[6]+"</td><td><a href='' onclick='eliminar("+unaFila[0]+")'>Eliminar</a></br><a href='' onclick='modificar("+unaFila[0]+")'>Modificar</a></td></tr>";
-                $('#tabla').append(contenidoHtml);                
-            }
-        }     
+        success: function(){}
     }
     );
     
